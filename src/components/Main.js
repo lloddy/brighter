2e382a2934c1d143350515663acb7a4dba2dd10b
Lloyd Eddy <lloyd@Lloyds-MacBook-Pro.local>
2022-01-13 12:40:43 -0500

    WIP connected to backend

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,48 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom'
 import Index from '../pages/Index';
 import Create from '../pages/Create';
 
 const Main = (props) => {
-    const [blogs, setBlogs] = useState([
+    const [blogs, setBlogs] = useState([]);
 
-    ])
+    const URL ='http://localhost:3001/blogs/'
+
+    const getBlogs = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setBlogs(data);
+    };
+
+    const createBlog = async (blog) => {
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(blog)
+        });
+        getBlogs();
+    };
+
+    const updateBlog = async (blog, id) => {
+        await fetch(URL + id, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(blog),
+        });
+        getBlogs();
+    };
+
+    const deleteBlog = async id => {
+        await fetch(URL + id, { method: "DELETE" });
+        getBlogs();
+    };
+
+    useEffect(() => getBlogs(), []);
+
     return (
         <div className="home">
             <Routes>
