9dd4a0876920f448172a617d1b2b529a7a72bb97
Lloyd Eddy <lloyd@Lloyds-MacBook-Pro.local>
2022-01-19 11:35:20 -0500

    WIP populates Index page

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,27 +7,14 @@ import db from '../firebase'
 import { collection, getDocs } from 'firebase/firestore'
 
 const Main = (props) => {
-    const [ blogs, setBlogs ] = useState([]);
-    const blogsCollectionRef = collection(db, "blogs")
-    useEffect(() => {
-        const getBlogs = async () => {
-            const data = await getDocs(blogsCollectionRef)
-            setBlogs(data.docs.map((doc) => ({...doc.data(), id: doc.id })))
-        }
-        getBlogs()
-    }, [])
+
 
     return (
-        <div>
-            {blogs.map((blog) => {
-                return (
-                    <div>
-                        {""}
-                        <h1>Title: {blog.title}</h1>
-                    </div>
-                )
-            })}
-        </div>
+        <main>
+            <Routes>
+                <Route path='/' element={<Index />}></Route>
+            </Routes>
+        </main>
     )
 }
 
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,13 +1,30 @@
-const Index = (props) => {
-    return (
-        props.blogs.map((blog) => (
-            <div key={blog._id}>
-                <h1 className="blogTitle">
-                    {blog.title}
-                </h1>
+import { useEffect, useState } from 'react';
+import db from '../firebase'
+import { collection, getDocs } from 'firebase/firestore'
 
-            </div>
-        ))
+const Index = (props) => {
+
+    const [ blogs, setBlogs ] = useState([]);
+    const blogsCollectionRef = collection(db, "blogs")
+    useEffect(() => {
+        const getBlogs = async () => {
+            const data = await getDocs(blogsCollectionRef)
+            setBlogs(data.docs.map((doc) => ({...doc.data(), id: doc.id })))
+        }
+        getBlogs()
+    }, [])
+
+    return (
+        <div>
+            {blogs.map((blog) => {
+                return (
+                    <div>
+                        {""}
+                        <h1>Title: {blog.title}</h1>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
