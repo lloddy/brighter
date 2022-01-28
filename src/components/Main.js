b91fb32c85ae19a3bcdfb9a8769910d87c4a6508
Lloyd Eddy <lloyd@Lloyds-MacBook-Pro.local>
2022-01-28 15:18:08 -0500

    WIP adds create route

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,11 +8,33 @@ import { collection, getDocs } from 'firebase/firestore'
 
 const Main = (props) => {
 
+    const [ blogs, setBlogs ] = useState([]);
+    const blogsCollectionRef = collection(db, "blogs")
+    useEffect(() => {
+        const getBlogs = async () => {
+            const data = await getDocs(blogsCollectionRef)
+            setBlogs(data.docs.map((doc) => ({...doc.data(), id: doc.id })))
+        }
+        getBlogs()
+    }, [])
+
+    const createBlog = async (blogs)  => {
+        await fetch(blogsCollectionRef, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(blogs)
+        })
+        // getBlogs();
+    }
+
 
     return (
         <main>
             <Routes>
                 <Route path='/' element={<Index />}></Route>
+                <Route path='/create' element={<Create />}></Route>
             </Routes>
         </main>
     )
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,53 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
 const Create = (props) => {
+    const navigate = useNavigate();
+    const [ newForm, setNewForm ] = useState({
+        title: "",
+        desc: "",
+    })
+
+    const handleChange = (event) => {
+        setNewForm(prevState => ({...prevState,
+        [event.target.name]: event.target.value }))
+    };
+
+    const handleSubmit = (event) => {
+        event. preventDefault();
+        props.createBlog(newForm);
+        setNewForm({
+            title: "",
+            desc: "",
+        })
+        navigate.push("/");
+    };
+
     return (
-        <h1>Create</h1>
+        <div>
+            <h1>Create</h1>
+            <section>
+                <form onSubmit={handleSubmit} >
+                    <div className="newForm">
+                        <input 
+                            type="text" 
+                            value={newForm.title}
+                            name="title"
+                            placeholder='Title'
+                            onChange={handleChange}
+                        />
+                        <input 
+                            type="textarea" 
+                            value={newForm.desc}
+                            name="desc"
+                            placeholder='desc'
+                            onChange={handleChange}
+                        />
+                        <input type="submit" value="Post" className="submit" />
+                    </div>
+                </form>
+            </section>
+        </div>
     )
 }
 
